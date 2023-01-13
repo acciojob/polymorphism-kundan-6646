592fdb5215124009fda8e9dfbcbe4a18be6a2f18
unknown <kundanjha826@gmail.com>
2023-01-13 14:48:53 +0530

    soultion-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(2, 4));
+        System.out.println(p.product(2, 4, 6));
+        System.out.println(p.product(2.7, 4.564));
+    }
 
+    public static class Product {
+        public static int product(int x, int y) {
+            return x*y;
+        }
+        public static int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public static double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
